ef67a9dd7212dbf099fd567674d0fe55e3445772
fhaze <eder_arm@icloud.com>
2022-01-30 15:49:57 -0300

    go test

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"nodejs-actions/message"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, message.HelloPerson("1.0.1"))
 	})
 	e.Logger.Fatal(e.Start(":3000"))
 }
